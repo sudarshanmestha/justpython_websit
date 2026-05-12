a040152462977decd37bbad6dff6ccd046e432d0
sudarshanmestha <sudarshan15399@gmail.com>
2026-05-12 11:45:45 +0530

    user_auth

--- vercelapp_backend/dapp/post_app/urls.py
+++ vercelapp_backend/dapp/post_app/urls.py
@@ -3,7 +3,7 @@ from .views import PostListView, PostCreateView, RetrieveAPIView
 
 
 
-app_name = "post_app"
+app_name = "post_appt_app"
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('create/', PostCreateView.as_view(), name='post-create'),
